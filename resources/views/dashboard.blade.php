0b451f91e9b26af62d02fd49e8375667d88babb4
petersonhz25 <mahez.pradana2070@students.unila.ac.id>
2024-03-26 09:56:26 +0700

    add: lazy load img

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -22,7 +22,7 @@
                             <div
                                 class="flex flex-col justify-center items-center bg-blue-600 rounded-t-xl overflow-hidden">
 
-                                <img src="{{asset('storage/' . $p->sampul)}}" alt="sampul">
+                                <img src="{{asset('storage/' . $p->sampul)}}" alt="sampul" loading="lazy">
                             </div>
                             <div class="p-4 md:p-6 max-h-96">
         <span class="block mb-1 text-xs font-semibold uppercase text-red-600 dark:text-blue-500">
@@ -40,8 +40,8 @@
                             <div
                                 class="mt-auto flex border-t border-gray-200 divide-x divide-gray-200 dark:border-gray-700 dark:divide-gray-700">
                                 <a class="w-full py-3 px-4 inline-flex justify-center items-center gap-x-2 text-sm font-medium rounded-es-xl bg-white text-gray-800 shadow-sm hover:bg-gray-50 disabled:opacity-50 disabled:pointer-events-none dark:bg-slate-900 dark:border-gray-700 dark:text-white dark:hover:bg-gray-800 dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600"
-                                   href="#">
-                                    View sample
+                                   href="{{route('pelatihan.show', $p->slug)}}">
+                                    View
                                 </a>
                                 <a class="w-full py-3 px-4 inline-flex justify-center items-center gap-x-2 text-sm font-medium rounded-ee-xl bg-white text-gray-800 shadow-sm hover:bg-gray-50 disabled:opacity-50 disabled:pointer-events-none dark:bg-slate-900 dark:border-gray-700 dark:text-white dark:hover:bg-gray-800 dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600"
                                    href="#">
