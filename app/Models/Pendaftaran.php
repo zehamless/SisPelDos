38ce04e1a4fc3c8de8d67720c2c7aaeceabec11b
petersonhz25 <mahez.pradana2070@students.unila.ac.id>
2024-06-06 14:21:11 +0700

    add: pendaftaran resource

--- app/Models/Pendaftaran.php
+++ app/Models/Pendaftaran.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\Pivot;
 
@@ -11,4 +12,17 @@ class Pendaftaran extends Pivot
     protected $table = 'daftarPeserta';
     protected $foreignKey = 'users_id';
     protected $relatedKey = 'pelatihan_id';
+
+    public function scopeMendaftar(Builder $query): Builder
+    {
+        return $query->whereIn('status', ['pending', 'diterima', 'ditolak']);
+    }
+    public function user()
+    {
+        return $this->belongsTo(User::class, 'users_id');
+    }
+    public function pelatihan()
+    {
+        return $this->belongsTo(Pelatihan::class, 'pelatihan_id');
+    }
 }
