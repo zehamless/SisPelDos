bd9ffecd70b63dab4c7b2920f4712092e4e922cd
petersonhz25 <mahez.pradana2070@students.unila.ac.id>
2024-06-14 22:30:04 +0700

    add: megaphone icon

--- resources/views/filament/user/widgets/running-pengumuman.blade.php
+++ resources/views/filament/user/widgets/running-pengumuman.blade.php
@@ -1,9 +1,11 @@
 <x-filament-widgets::widget>
     <x-filament::section>
-                <marquee>
-                    @foreach ($messages as $message)
-                        {{ $message }}
-                    @endforeach
-                </marquee>
+        <marquee>
+            <x-heroicon-o-megaphone class="h-5 w-5 inline-block" />
+            @foreach ($messages as $message)
+                {{ $message }}
+            @endforeach
+            <x-heroicon-o-megaphone class="h-5 w-5 inline-block" />
+        </marquee>
     </x-filament::section>
 </x-filament-widgets::widget>
